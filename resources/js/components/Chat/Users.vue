2ad80feb5dfa2c5e186b7091be6171e59611ed53
Steve Idowu <idowusteve@ymail.com>
2019-07-21 01:41:40 +0100

    Reatime setup and showing online users

--- resources/js/components/Chat/Users.vue
+++ resources/js/components/Chat/Users.vue
@@ -1,22 +1,55 @@
 <template>
-    <div class="users">
-       <div class="users__header">1 user online</div>
+    <div class="users" v-if="users.length">
+       <div class="users__header">{{ users.length }} user{{ users.length > 1 ? 's' : '' }} online</div>
 
-       <div class="users__user">
-           <a href="#">Steve Idowu</a>
+       <div class="users__user" v-for="user in users" :key="user.id">
+           <a href="#"><span class="bull">&bull;</span> {{ user.name }}</a>
        </div>
     </div>
 </template>
 <script>
+import Bus from '../../bus'
+
 export default {
-    
+    data() {
+        return {
+            users: []
+        }
+    },
+    methods: {
+        removeUser(id) {
+            this.users = this.users.filter((user) => {
+                return user.id !== id              
+            })
+                
+        },
+
+        addUser(user)
+        {
+            this.users.unshift(user)
+        }
+    },
+    mounted() {
+        Bus.$on('users.here', (users) => {
+            this.users = users
+        })
+        .$on('user.joined', (user) => {
+           this.addUser(user)
+        })
+        .$on('user.left', (user) => {
+            this.removeUser(user.id)
+        })
+    },
 }
 </script>
 
 <style lang="scss">
+    .bull{
+        color:green;
+    }
     .users {
         background-color: #ffffff;
-        border: 1px solid #d3e0e9;
+        border: 1px solid #323333;
         border-radius: 3px;
 
         &__header {
